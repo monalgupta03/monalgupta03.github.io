9292a1e2265c3aed8508d1268e6ee477f6ca1949
Monal Gupta <monalmehal@gmail.com>
2026-02-11 15:09:30 +0000

    Change base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 
-  base: "/",
+  base: "/monalgupta03.github.io",
   
   server: {
     host: "::",
