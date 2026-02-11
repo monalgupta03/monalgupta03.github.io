3582a16fd5e788a0f58c7c5ff68cd3f4343e0837
Monal Gupta <monalmehal@gmail.com>
2026-02-11 15:14:08 +0000

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 
-  base: "/monalgupta03.github.io",
+  base: "/",
   
   server: {
     host: "::",
